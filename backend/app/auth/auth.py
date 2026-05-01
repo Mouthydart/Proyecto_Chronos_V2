11ee294320298bee851f9b2e042de51cb8167a68
Santiago Hernandez <sannuher@gmail.com>
2026-05-01 13:24:20 -0500

    David y Santiago

--- backend/app/auth/auth.py
+++ backend/app/auth/auth.py
@@ -1,4 +1,4 @@
-#Seguridad (hashear contrasena, crear token, verificar tokens)
+#Seguridad (hashear contrasena, crear token, verificar token)
 
 from datetime import datetime, timedelta
 from typing import Optional
