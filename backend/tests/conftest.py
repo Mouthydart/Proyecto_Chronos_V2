4d4ff7cf8caf64f3587ec2e4b8265daaff8eb5b5
David13-117 <d.castillom55@gmail.com>
2026-05-31 10:48:18 -0500

    test_1

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 import mongomock
-from backend.main import app
+from main import app
 from app.database.mongodb import mongodb
 
 @pytest.fixture(scope="session", autouse=True)
