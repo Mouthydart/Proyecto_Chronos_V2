c88ce3fcbf0aef58f9217e654fed90302e98950f
David Castillo <d.castillom55@gmail.com>
2026-06-01 19:43:30 -0500

    Cambio API Vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8000/api'
+//const API_BASE_URL = 'http://localhost:8000/api'
+// BIEN (Detecta automáticamente si estás en producción o en local)
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 // Crear instancia de Axios con configuración base
 const api = axios.create({
