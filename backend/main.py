9a54e6ae5946fbb52798bcdcade6d875f5832eb6
Santiago Hernandez <sannuher@gmail.com>
2026-06-04 19:23:50 -0500

    Configuracion CORS

--- backend/main.py
+++ backend/main.py
@@ -19,8 +19,8 @@ app = FastAPI(title="Chronos API", version="1.0.0", lifespan=lifespan, redirect_
 origins = [
     "http://localhost:5173",          # Tu Vue.js en local (PC)
     "http://127.0.0.1:5173",          # Alternativa local
-    "https://chronos-copy-v2.vercel.app", # <-- ¡Tu URL real de Vercel!
-    "https://chronos-copy-v2.vercel.app/"
+    "https://proyecto-chronos-v2", # <-- ¡Tu URL real de Vercel!
+    "https://proyecto-chronos-v2/"
 ]
 
 app.add_middleware(
